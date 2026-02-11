f54cc8378ac1870174b9e9b5cfed5f65a7989817
asoeb <a.soeborg-skau@hotmail.dk>
2026-02-11 11:47:29 +0100

    Added /api/logout endpoint

--- go_app/backend/main.go
+++ go_app/backend/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"html/template" // templating-pakke i go
-	"net/http"      // http-pakke i go
+	"encoding/json" // Needed for endpoints
+	"html/template" // templating-pakke in go
+	"net/http"      // http-pakke in go
 )
 
 var testTemplate = template.Must(template.ParseFiles("templates/test.html"))
@@ -13,16 +14,46 @@ func main() {
 	// Take incoming requests and dispatch them to the matching handlers
 	mux := http.NewServeMux()
 
-	// Register the routes and handlers
-	mux.Handle("/", &homeHandler{})
+	// GET / - Serve Root Page
+	mux.Handle("/", &rootHandler{})
+
+	// GET /register - Serve Register Page
+	// TODO: Implement ^
+
+	// GET /login - Serve Login Page
+	// TODO: Implement ^
+
+	// GET /api/search - Search
+	// TODO: Implement ^
+
+	// POST /api/register - Register
+	// TODO: Implement ^
+
+	// POST /api/login - Login
+	// TODO: Implement ^
+
+	// GET /api/logout - Logout
+	mux.Handle("/api/logout", &logoutHandler{})
 
 	// Run the server on port :8080
 	http.ListenAndServe(":8080", mux)
 }
 
-type homeHandler struct{}
+type rootHandler struct{}
 
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Render template
+func (h *rootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Serve Root Page
 	testTemplate.Execute(w, nil)
+}
+
+type logoutHandler struct{}
+
+func (h *logoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// JSON-response
+	resp := map[string]string{
+		"status": "logged out",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
