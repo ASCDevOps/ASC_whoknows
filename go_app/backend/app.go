1b5ad6176c3557d3f2a76b75bc80392d258c8ead
simon sivebaek <simonsivebaek@protonmail.com>
2026-02-11 20:42:57 +0100

    commits messages do not matter up till this one have had problems with commiting on Vs code

--- go_app/backend/app.go
+++ go_app/backend/app.go
@@ -1,5 +1,9 @@
+//package main is needed to  make it runable 
+
 package main
 
+
+//imports database and should work if we decide to move over to SQL
 import (
 	"database/sql"
 	"fmt"
@@ -9,15 +13,20 @@ import (
 )
 
 func main(){
+	
+	//opens whoknows.db if null creates whoknows.db
 	db, err :=sql.Open("sqlite", "file:whoknows.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	//error handling
 	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 	}
 
+
+	//print so we know if database is connected 
 	fmt.Println("SQLite connected!")
 }
