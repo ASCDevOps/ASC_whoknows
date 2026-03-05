06907627ff1399a161fa2218daf4534c3fff1512
Simon <simonsivebaek.hotmail@gmail.com>
2026-03-05 13:02:39 +0100

    Register edited but still not ready

--- go_app/backend/handlers/register.go
+++ go_app/backend/handlers/register.go
@@ -3,17 +3,55 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"database/sql"
+	"strings"
 )
 
-type RegisterHandler struct{}
+type RegisterHandler struct{
+	DB *sql.DB
+}
 
 var registerTemplate = template.Must(template.ParseFiles("templates/test.html"))
 
-func (*RegisterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+func (h *RegisterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	_ = registerTemplate.Execute(w, nil)
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid form", http.StatusBadRequest)
+		return	
+	}
+
+	username := strings.TrimSpace(r.FormValue("username"))
+	email := strings.TrimSpace(r.FormValue("email"))
+	password := strings.TrimSpace(r.FormValue("password"))
+	password1 := strings.TrimSpace(r.FormValue("password1"))
+
+	if username == "" || email == "" || password == "" {
+		http.Error(w, "missing fields", http.StatusBadRequest)
+		return
+	}
+
+	if password != password1 {
+		http.Error(w, "Password do not match", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.DB.Exec(
+		"INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+		username, email, password,
+	)
+
+	if err != nil{
+		http.Error(w, "User creation failed!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"statusCode":200,"message":"User registered"}`))
 }
+
+
